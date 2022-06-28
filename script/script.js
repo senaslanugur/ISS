b3983756fdf35543c0294c9cc2caa503402f069f
Uğur Şenaslan <91412306+senaslanugur@users.noreply.github.com>
2022-06-28 19:31:03 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
+// var map = L.map('map').setView([0,0], 4);
+// var basemap = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?access-token=PyTJUlEU1OPJwCJlW1k0NC8JIt2CALpyuj7uc066O7XbdZCjWEL3WYJIk6dnXtps', {
+//   attribution: '',
+//   minZoom: 2,
+//   maxZoom: 16,
+// 	className: 'map-tiles'
+// });
+
 var map = L.map('map').setView([0,0], 4);
 var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?access-token=PyTJUlEU1OPJwCJlW1k0NC8JIt2CALpyuj7uc066O7XbdZCjWEL3WYJIk6dnXtps', {
   attribution: '',
@@ -5,13 +13,6 @@ var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?ac
   maxZoom: 16,
 	className: 'map-tiles'
 });
-
-// var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   attribution: 'Basemap (c) <a href="http://openstreetmap.org">OpenStreetMap</a>',
-//   minZoom: 2,
-//   maxZoom: 16,
-// 	className: 'map-tiles'
-// });
 
 basemap.addTo(map);
 
@@ -37,22 +38,31 @@ var latitude,longitude;
 
 setInterval(()=>{
 
-	var xmlhttp = new XMLHttpRequest();
+	// var xmlhttp = new XMLHttpRequest();
+  //
+	// xmlhttp.onreadystatechange = function () {
+	//     if (this.readyState == 4 && this.status == 200) {
+	//         var myArr = JSON.parse(this.responseText);
+	// 				// console.log(JSON.parse(myArr.contents).iss_position)
+  //         // var position = JSON.parse(myArr.contents).iss_position
+	// 				latitude = myArr.iss_position.latitude;
+	// 				longitude = myArr.iss_position.longitude;
+  //
+	// 				updatemap();
+	//     }else{}
+	// };
+  //
+	// xmlhttp.open('GET','http://api.open-notify.org/iss-now.json',false);
+  // // xmlhttp.open('GET', document.location.protocol + '//api.allorigins.win/get?url='+escape("http://api.open-notify.org/iss-now.json", true),false);
+	// xmlhttp.send();
 
-	xmlhttp.onreadystatechange = function () {
-	    if (this.readyState == 4 && this.status == 200) {
-	        var myArr = JSON.parse(this.responseText);
-					console.log(myArr.iss_position)
-
-					latitude = myArr.iss_position.latitude;
-					longitude = myArr.iss_position.longitude;
-
-					updatemap();
-	    }else{}
-	};
-
-	xmlhttp.open('GET','http://api.open-notify.org/iss-now.json',false);
-	xmlhttp.send();
+  $.getJSON('http://api.open-notify.org/iss-now.json', (data) => {
+    console.log(data);
+    var myArr = data;
+		latitude = myArr.iss_position.latitude;
+		longitude = myArr.iss_position.longitude;
+    updatemap();
+  });
 
 
 }, 2500)
